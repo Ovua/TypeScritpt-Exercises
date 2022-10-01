37eaf2c256f6098c0f2f1811c4cd0b49ef3ee9ca
Ivan <97966255+Ovua@users.noreply.github.com>
2022-10-01 10:04:35 +0200

    ADD feat/ts

---
+++ exercise-1-construct-types-from-types-start.js
@@ -0,0 +1,36 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    code: "CN",
+    population: 1412600000
+};
+var countryB = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+    population: 10678632,
+    currency: "Euro"
+};
+var countryC = {
+    name: "Italy",
+    code: "IT",
+    languages: ["Italian"],
+    population: 60317116,
+    currency: "Euro"
+};
+console.log(countryC);
+var countryD = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5135300,
+    currency: "Dollar"
+};
+var countryE = {
+    name: "Thailand",
+    code: "TH",
+    languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+    currency: "Baht"
+};
